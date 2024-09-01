21de9385c7ee26df65a742e45ed0f87d4b1bcc4c
Ram <gajjalaramprasadreddy@gmail.com>
2024-08-31 20:42:21 -0500

    initial commit

---
+++ jobjot_backend/src/main/java/com/ram/jobjot/jobjot/JobjotApplication.java
@@ -0,0 +1,13 @@
+package com.ram.jobjot.jobjot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobjotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobjotApplication.class, args);
+	}
+
+}
---
+++ jobjot_backend/src/test/java/com/ram/jobjot/jobjot/JobjotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.jobjot.jobjot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobjotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

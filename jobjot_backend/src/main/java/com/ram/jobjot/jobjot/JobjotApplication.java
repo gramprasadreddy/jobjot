02a747258ae790b0b80f781f62d789c45dd14417
Ram <gajjalaramprasadreddy@gmail.com>
2024-10-23 17:22:45 -0500

    initial commit

--- jobjot_backend/src/main/java/com/ram/jobjot/jobjot/JobjotApplication.java
+++ jobjot_backend/src/main/java/com/ram/jobjot/jobjot/JobjotApplication.java
@@ -2,12 +2,22 @@ package com.ram.jobjot.jobjot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class JobjotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobjotApplication.class, args);
-	}
+		ApplicationContext ctx = SpringApplication.run(JobjotApplication.class, args);
 
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
 }
---
+++ jobjot_backend/src/main/java/com/ram/jobjot/jobjot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ram.jobjot.jobjot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello World Spring boot!";
+	}
+
+}
